053c1bcd5466e168737d4d682b36c0365813c5d3
antoninhrlt <antoninhrlt@proton.me>
2023-08-09 16:32:01 +0200

    Exception catch when converting images

--- lib/utils/image.dart
+++ lib/utils/image.dart
@@ -1,16 +1,24 @@
 import 'dart:async';
 import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
 
 extension ToDartImage on Image {
   /// Obtient une [ui.Image] à partir d'une [Image].
-  Future<ui.Image> toDartImage() async {
+  /// [onError] est appelé si une erreur survient lors de la conversion et permet de renvoyer une image par défaut.
+  Future<ui.Image> toDartImage({Image Function()? onError}) async {
     var completer = Completer<ImageInfo>();
 
-    image.resolve(const ImageConfiguration()).addListener(ImageStreamListener((info, _) {
-      completer.complete(info);
-    }));
+    try {
+      image.resolve(const ImageConfiguration()).addListener(ImageStreamListener((info, _) {
+        completer.complete(info);
+      }));
+    } catch (e) {
+      completer.completeError(AssertionError(e.toString()));
+      if (onError != null) {
+        return onError().toDartImage();
+      }
+      rethrow;
+    }
 
     ImageInfo imageInfo = await completer.future;
     return imageInfo.image;
