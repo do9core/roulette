7f1ea3b1cc1c2f1a908d1c26395810b57c55c4be
do9core <do9core@outlook.com>
2026-03-03 10:23:37 +0800

    fix: Update roulette.dart to export SectionImageLayout along with RouletteStyle

--- lib/roulette.dart
+++ lib/roulette.dart
@@ -20,4 +20,4 @@ export './src/roulette.dart' show Roulette;
 export './src/roulette_controller.dart' show RouletteController;
 export './src/roulette_group.dart' show RouletteGroup;
 export './src/roulette_unit.dart' show RouletteUnit;
-export './src/roulette_style.dart' show RouletteStyle;
+export './src/roulette_style.dart' show RouletteStyle, SectionImageLayout;
