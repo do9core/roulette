6c067606e5d3cca3bd4893fd00c2747217bf6006
do9core <do9core@outlook.com>
2026-08-13 15:14:17 +0800

    feat: fix logic test, add material_ui as test dependency

---
+++ test/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+Color transparent(int index) {
+  return const Color.fromRGBO(0, 0, 0, 0);
+}
+
+Color black(int index) {
+  return const Color.fromRGBO(255, 255, 255, 1);
+}
